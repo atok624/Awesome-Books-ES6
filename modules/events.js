c952ef32a5786baccb16abc4fcb025200e54b029
atok624 <84607674+atok624@users.noreply.github.com>
2023-02-20 23:51:21 +0000

    Create events.js file

---
+++ modules/events.js
@@ -0,0 +1,22 @@
+const list = document.querySelector('.List');
+  const add = document.querySelector('.add-new');
+  const contact = document.querySelector('.contact');
+  const listLink = document.getElementById('l-link');
+  const addLink = document.getElementById('a-link');
+  const contactLink = document.getElementById('c-link');
+  
+  listLink.addEventListener('click', () => {
+    list.style.display = 'block';
+    contact.style.display = 'none';
+    add.style.display = 'none';
+  });
+  addLink.addEventListener('click', () => {
+    add.style.display = 'block';
+    contact.style.display = 'none';
+    list.style.display = 'none';
+  });
+  contactLink.addEventListener('click', () => {
+    add.style.display = 'none';
+    contact.style.display = 'flex';
+    list.style.display = 'none';
+  });
